11425ce9a59d93987b5164ed74609bd3715f28b4
Romesh Jain <romeshjainofficial@gmail.com>
2024-04-21 18:14:40 +0530

    update

---
+++ ToDoList/backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors);
+app.use(express.json());
+
+mongoose.connect("mongodb:127.0.0.1:27017/todoapp");
+//localhost:27017
+
+mongodb: app.post("/add", (req, res) => {
+  const task = req.body.task;
+});
+
+app.listen(3001, () => console.log("Server is up"));
